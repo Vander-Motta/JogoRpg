369b31a5fb8d74c16aa06ba46080cd34c1ae0517
Vanderson Manoel Motta Gonçalves <107154217+Vander-Motta@users.noreply.github.com>
2022-08-02 16:06:18 -0300

    Adicionado o personagem

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const canvas = document.querySelector('canvas'); //constante que seleciona a tela
 const c = canvas.getContext('2d');
 
-canvas.width = 1333 ;
-canvas.height = 776;
+canvas.width = 1024 ;
+canvas.height = 576;
 
 c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const personagemjogador = new Image()
+personagemjogador.src='./Imagens/playerDown.png'
+
 const Imagem = new Image()
 Imagem.src = './Imagens/maparpg.png'
 
-console.log(Imagem)
+
+
 
 Imagem.onload = () => {
-    c.drawImage(Imagem, -400, -700)
+    c.drawImage(Imagem, -217, -860)
+    c.drawImage(personagemjogador,
+                0,
+                0,
+                personagemjogador.width / 4,
+                personagemjogador.height,
+                
+                canvas.width / 2 - (personagemjogador.width / 4) / 2,
+                canvas.height  / 2 - personagemjogador.height / 2,
+
+
+                personagemjogador.width / 4,
+                personagemjogador.height
+                
+                 
+        )
 }
 // Este Primeiro Bloco se trata da configuração do Canvas e a importação do Mapa para dentro do Projeto
 
